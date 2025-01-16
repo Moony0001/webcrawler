eec2b7490c02f8062800d4c95c2647e1a7107543
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2025-01-16 23:12:47 +0530

    getURLsFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,7 @@
-const {normalizeURL} = require('./crawl.js');
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js');
 const { test, expect } = require("@jest/globals");
 
+//Tests for normalizeURL
 test("normalizeURL strip protocol", () => {
     const input = 'https://example.com/path';
     const actual = normalizeURL(input);
@@ -26,5 +27,62 @@ test("normalizeURL capitals", () => {
     const input = 'https://EXAMPLE.com/path/';
     const actual = normalizeURL(input);
     const expected = 'example.com/path';
+    expect(actual).toEqual(expected);
+})
+
+//Tests for getURLsFromHTML
+test("getURLsFromHTML absolute", () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="https://example.com/path/">Link</a>
+            </body>
+        </html>
+    `;
+    const inputBaseURL = 'https://example.com';
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://example.com/path/"];
+    expect(actual).toEqual(expected);
+})
+test("getURLsFromHTML relative", () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="/path/">Link</a>
+            </body>
+        </html>
+    `;
+    const inputBaseURL = 'https://example.com';
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://example.com/path/"];
+    expect(actual).toEqual(expected);
+})
+test("getURLsFromHTML multiple links of both type", () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="https://example.com/path1/">Link</a>
+                <a href="/path2/">Link</a>
+            </body>
+        </html>
+    `;
+    const inputBaseURL = 'https://example.com';
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://example.com/path1/", "https://example.com/path2/"];
+    expect(actual).toEqual(expected);
+})
+test("getURLsFromHTML invalid links of both type", () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="https://example.com/path1/">Link</a>
+                <a href="/path2/">Link</a>
+                <a href="invalid">Link</a>
+            </body>
+        </html>
+    `;
+    const inputBaseURL = 'https://example.com';
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://example.com/path1/", "https://example.com/path2/"];
     expect(actual).toEqual(expected);
 })
