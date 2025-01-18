744832f7f331f07afd38ec92ae479c833465aba9
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2025-01-18 19:42:19 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,25 +1,47 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL){
-    console.log(`actively crawling: ${currentURL}`);
+async function crawlPage(baseURL, currentURL, pages){
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+    if(baseURLObj.hostname !== currentURLObj.hostname){
+        return pages;
+    }
 
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    if(pages[normalizedCurrentURL]>0){
+        pages[normalizedCurrentURL]++;      //If we have already seen this URL, we increment the count. This is useful when we want to generate a report on how many times a page was linked to a URL.
+        return pages;
+    }
+
+    pages[normalizedCurrentURL] = 1;    //If we haven't seen this URL before, we add it to the pages object and set the count to 1.
+
+    console.log(`actively crawling: ${currentURL}`);    //We log the URL we are actively crawling. We don't log the URLs we have already seen because we don't want to log the same URL multiple times.
     try {
         const resp = await fetch(currentURL);
         if(resp.status > 399){
             console.log(`Error in fetching ${currentURL} with status code: ${resp.status}`);
-            return;
+            return pages;
         }
 
         const contentType = resp.headers.get("content-type");
         if(!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType} on page: ${currentURL}`);
-            return;
+            return pages;
         }
 
-        console.log(await resp.text());     //await-ing it because we want to wait for the response to come back before we try to read the text from it.
+        const htmlBody = await resp.text();
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages);
+        }
+
     } catch (error) {
         console.log(`error fetching ${currentURL}: ${error.message}`); 
     }
+
+    return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL){
